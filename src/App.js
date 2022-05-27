fabcfd0dc75de874ab6ab059494bd7bd3681a5d3
milannathwani07 <87632948+milannathwani07@users.noreply.github.com>
2022-05-27 17:16:21 +0530

    made pokemon api

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+import Axios from"axios";
+function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+     name: "", 
+      species: "", 
+      img: "", 
+      hp: "",
+      attack: "",
+      defence: "",
+      type: "",
+  })
+  const searchPokemon = () => {
+   Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response)=> {
+     setPokemon(
+      {name: pokemonName, 
+      species: response.data.species.name, 
+      img: response.data.sprites.front_default, 
+      hp: response.data.stats[0].base_stat,
+      attack: response.data.stats[1].base_stat,
+      defence: response.data.stats[2].base_stat,
+      type: response.data.types[0].type.name,
+    
+    });
+    setPokemonChosen(true);
+   })
+  }
+  return (
+    <div className='App'>
+      <div className="Title">
+      <h1>Pokemon Stats</h1>
+      <input type="text" onChange={(event) => {setPokemonName(event.target.value);}} />
+      <button onClick={searchPokemon}>
+        Search Pokemon
+      </button>
+      </div>
+      <div className="DisplaySection">
+       {!pokemonChosen ? (<h1>Please chose a Pokemon</h1>) : 
+       (
+       <>
+       <h1>{pokemon.name}</h1>
+       <img src='pokemon.img'/>
+       <h3>Species: {pokemon.species}</h3>
+       <h3>Type: {pokemon.type}</h3>
+       <h4>Hp: {pokemon.hp}</h4>
+       <h4>Attack: {pokemon.attack}</h4>
+       <h4>Defence: {pokemon.defence}</h4>
+       </>
+       ) }
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
